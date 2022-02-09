c4d2138c17069d9c1ebecdc13b2a287ccfe9e7d3
Chilo Nwokolo <chilo.nwokolo@gmail.com>
2022-02-09 13:50:17 +0100

    remove middleware file

--- pages/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextFetchEvent, NextRequest } from 'next/server';
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log("request:", req);
-  console.log("ev:", ev);
-}
